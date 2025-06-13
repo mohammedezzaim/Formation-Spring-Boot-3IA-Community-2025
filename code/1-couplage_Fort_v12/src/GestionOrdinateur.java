0ef79a1a95f52208d880c8dd810428f2badba995
Mohammed Ezzaim <mohammedezzaim00@gmail.com>
2025-06-13 12:40:21 +0100

    cours of IOC Container

--- code/1-couplage_Fort_v12/src/GestionOrdinateur.java
+++
@@ -1,9 +0,0 @@
-/**
- * @author Ezzaim Mohammed
- **/
-public class GestionOrdinateur {
-    public static void main(String[] args) {
-        Ordinateur ordinateur = new Ordinateur();
-        ordinateur.utiliserPortUSB();
-    }
-}
