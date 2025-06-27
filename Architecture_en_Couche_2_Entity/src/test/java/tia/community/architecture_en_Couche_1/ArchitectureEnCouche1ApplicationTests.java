8f44cb124b3773515df6902b7a6df594b814e262
Mohammed Ezzaim <mohammedezzaim00@gmail.com>
2025-06-27 12:06:41 +0100

    couche Entity spring v25

---
+++ Architecture_en_Couche_2_Entity/src/test/java/tia/community/architecture_en_Couche_1/ArchitectureEnCouche1ApplicationTests.java
@@ -0,0 +1,13 @@
+package tia.community.architecture_en_Couche_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchitectureEnCouche1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
