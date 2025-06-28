abc409cb438f0c51ff493c4f2342f48af911754b
Mohammed Ezzaim <mohammedezzaim00@gmail.com>
2025-06-28 18:20:54 +0100

    code of ioc container

---
+++ ioc_container/src/main/java/com/example/firstProject/Dev.java
@@ -0,0 +1,15 @@
+package com.example.firstProject;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Ezzaim Mohammed
+ **/
+
+@Component("dev_3ia")
+public class Dev {
+    public void build() {
+        System.out.println("Hello, 3IA Community!");
+    }
+}
+
