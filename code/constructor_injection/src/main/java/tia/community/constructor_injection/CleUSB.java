570730745539c81590a02434ff8178f73b74cb8b
Mohammed Ezzaim <mohammedezzaim00@gmail.com>
2025-06-28 18:06:41 +0100

    constructeur injection

--- code/constructor_injection/src/main/java/tia/community/constructor_injection/CleUSB.java
+++ code/constructor_injection/src/main/java/tia/community/constructor_injection/CleUSB.java
@@ -4,7 +4,8 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 /**
- * @author Ezzaim Mohammed
+ * @author Ezzaim Mohammed 
+ * 3IA Community 
  **/
 
 @Component
