bcd4eb607837f4b8fc5e6a4f3625ab4649e17813
mohammedezzaim <mohammedezzaim00@gmail.com>
2026-01-17 01:48:23 +0100

    video 49 CRUD --> methode Read findAll() findByName() findByCategory

--- couch-service-demo2/src/main/java/tia/community/couch_service_demo1/CouchServiceDemo1Application.java
+++ couch-service-demo2/src/main/java/tia/community/couch_service_demo1/CouchServiceDemo1Application.java
@@ -16,37 +16,37 @@ public class CouchServiceDemo1Application {
 		ApplicationContext ioc_container = SpringApplication.run(CouchServiceDemo1Application.class, args);
 		ProductService productService = ioc_container.getBean(ProductService.class);
 
-//		Product lenovo = new Product("Lenovo T14", 10000.0, "Laptop", true, 10, LocalDate.now());
-//		int check = productService.save(lenovo);
-//		System.out.println(check);
+		Product lenovo = new Product("Lenovo T14", 10000.0, "Laptop", true, 10, LocalDate.now());
+		int check = productService.save(lenovo);
+		System.out.println(check);
 
-//		List<Product> products = productService.findAll();
-//
-//		products.forEach(product -> {
-//			System.out.println("--------------------------");
-//			System.out.println(product.getId());
-//			System.out.println(product.getName());
-//			System.out.println(product.getPrice());
-//			System.out.println(product.getCategory());
-//		});
+		List<Product> products = productService.findAll();
 
-//
-//		Product lenovoT14 = productService.findByName("Lenovo T15");
-//		System.out.println("--------------------------");
-//		System.out.println(lenovoT14.getId());
-//		System.out.println(lenovoT14.getName());
-//		System.out.println(lenovoT14.getPrice());
-//		System.out.println(lenovoT14.getCategory());
+		products.forEach(product -> {
+			System.out.println("--------------------------");
+			System.out.println(product.getId());
+			System.out.println(product.getName());
+			System.out.println(product.getPrice());
+			System.out.println(product.getCategory());
+		});
 
-//		List<Product> laptops = productService.findByCategory("Laptop");
-//		for (Product product : laptops) {
-//			System.out.println("--------------------------");
-//			System.out.println(product.getId());
-//			System.out.println(product.getName());
-//			System.out.println(product.getPrice());
-//			System.out.println(product.getCategory());
-//			System.out.println("--------------------------");
-//		}
+
+		Product lenovoT14 = productService.findByName("Lenovo T15");
+		System.out.println("--------------------------");
+		System.out.println(lenovoT14.getId());
+		System.out.println(lenovoT14.getName());
+		System.out.println(lenovoT14.getPrice());
+		System.out.println(lenovoT14.getCategory());
+
+		List<Product> laptops = productService.findByCategory("Laptop");
+		for (Product product : laptops) {
+			System.out.println("--------------------------");
+			System.out.println(product.getId());
+			System.out.println(product.getName());
+			System.out.println(product.getPrice());
+			System.out.println(product.getCategory());
+			System.out.println("--------------------------");
+		}
 
 		Product idEqualsOne = productService.findById(1);
 		System.out.println("--------------------------");
